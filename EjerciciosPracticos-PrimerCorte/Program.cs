2c8333e46a28e4587c731d0b8fe251731f2d7632
Bismarck Flores <bjflores@uamv.edu.ni>
2025-09-26 09:50:40 -0600

    Add project files.

---
+++ EjerciciosPracticos-PrimerCorte/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosPracticos_PrimerCorte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
